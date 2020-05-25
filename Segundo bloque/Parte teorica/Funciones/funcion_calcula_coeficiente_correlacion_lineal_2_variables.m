88f5c494f0a2bf54afce88edb1f6a419277f67f1
Ihar Myshkevich <ihar.myshkevich134@alu.uhu.es>
2020-05-25 11:29:31 +0200

    Fixes

--- Segundo bloque/Parte teorica/Funciones/funcion_calcula_coeficiente_correlacion_lineal_2_variables.m
+++ Segundo bloque/Parte teorica/Funciones/funcion_calcula_coeficiente_correlacion_lineal_2_variables.m
@@ -1,4 +1,4 @@
-function coef_corr = funcion_calula_coeficiente_correlacion_lineal_2_variables(mCov)
+function coef_corr = funcion_calcula_coeficiente_correlacion_lineal_2_variables(mCov)
 
     sigma1 = sqrt(mCov(1,1));
     sigma2 = sqrt(mCov(2,2));
