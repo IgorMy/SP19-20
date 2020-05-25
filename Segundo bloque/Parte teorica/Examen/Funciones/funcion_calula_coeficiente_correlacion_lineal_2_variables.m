578b97e3d583b4bcc9114c3cdce90caae84f917c
Ihar Myshkevich <ihar.myshkevich134@alu.uhu.es>
2020-05-25 10:12:21 +0200

    Pequeña edición

--- Segundo bloque/Parte teorica/Examen/Funciones/funcion_calula_coeficiente_correlacion_lineal_2_variables.m
+++
@@ -1,8 +0,0 @@
-function coef_corr = funcion_calula_coeficiente_correlacion_lineal_2_variables(mCov)
-
-    sigma1 = sqrt(mCov(1,1));
-    sigma2 = sqrt(mCov(2,2));
-    
-    coef_corr = mCov(1,2)/(sigma1*sigma2);
-
-end
