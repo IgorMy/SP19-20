59bb40671b2c40105dd90658a16391947610ac3c
Ihar Myshkevich <ihar.myshkevich134@alu.uhu.es>
2020-06-12 11:42:11 +0200

    Práctica para el examen (Ejemplo 1)

---
+++ Segundo bloque/Ejercicios_pre_examen/02_FaseEntrenamiento/Ejemplo1/01_MDE_CC_T/01_SeleccionDescriptores/MDE_CC_T.m
@@ -0,0 +1,50 @@
+clear,clc,close all, restoredefaultpath;
+%% Carga de datos
+
+addpath('Funciones')
+
+addpath('../../../../01_GeneracionDatos/DatosGenerados')
+load('conjunto_datos_estandarizados.mat')
+load('nombresProblema.mat')
+
+% otra forma de cargar los datos
+rutaFichero = '../../../../01_GeneracionDatos/DatosGenerados/';
+nombreFichero = 'conjunto_datos_estandarizados.mat';
+load([rutaFichero nombreFichero]);
+nombreFichero = 'nombresProblema.mat';
+load([rutaFichero nombreFichero]);
+
+clear nombreFichero rutaFichero;
+
+%% Seleccionamos las clases que deseamos
+
+X = Z;
+Y(Y==2) = 1;
+codifClases = unique(Y);
+nombresProblema.clases{1} = 'CirculosCuadrados';
+
+codifClasesOI = codifClases;
+
+filasOI = false(size(Y));
+for i=1:length(clasesOI)
+    filasOI_i = Y == codifClasesOI(i);
+    filasOI = or(filasOI,filasOI_i);
+end
+
+numDescriptores = size(X,2);
+
+XoI = X(filasOI,1:numDescriptores - 1); % quitamos el numero de euler
+YoI = Y(filasOI);
+
+%% Ejecutamos el algoritmo de seleccion de los mejores atributos
+numDescriptoresOI = 9;
+[espacioCcas, JespacioCcas]=funcion_selecciona_vector_ccas_3_dim(XoI,YoI,numDescriptoresOI);
+
+
+%% Guardamos los datos
+
+nombresProblemaIO = [];
+nombresProblemaIO.descriptores = nombresProblema.descriptores;
+nombresProblemaIO.clases = nombresProblema.clases(clasesOI);
+nombresProblemaIO.simbolos = nombresProblema.simbolos;
+save('DatosGenerados/MDE_CC_T','espacioCcas','nombresProblemaIO','XoI','YoI');
